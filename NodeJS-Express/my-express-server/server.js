11dcc791c05fb661441de2536c7ea088ac7c3974
pascaldevops <pascaldevops@gmail.com>
2019-03-07 11:33:58 -0500

    add more descriptive comments

--- NodeJS-Express/my-express-server/server.js
+++ NodeJS-Express/my-express-server/server.js
@@ -1,23 +1,33 @@
 //jshint esversion: 6
 
-// Create an express server
-const express = require('express');
-const app = express();
+// Loading the express module
+const express = require('express')
 
-// The location and stuff my server is serving
+// Create an express application
+const app = express()
+
+// create a (root) route to the home page
+// req is the request the express server receives from the user via the browser
+// res is the response the server sends backs to the browser
+// The user type: localhost:3000
+// The express server sends back: Hello World!!!
 app.get('/', function(req, res) {
-  res.send('<h1>Hello World!!!</h1>');
-});
+  res.send('<h1>Hello World!!!</h1>')
+})
 
+// creatye a contact route
 app.get('/contact', function(req, res) {
-  res.send('<h3>Contact me at done@gmail.com</h3>');
-});
+  res.send('<h3>Contact me at done@gmail.com</h3>')
+})
 
+// create an about route
 app.get('/about', function(req, res) {
-  res.send('<p>I am an aspiring web developer. I am passionate about robots.</p>');
-});
+  res.send(
+    '<p>I am an aspiring web developer. I am passionate about robots.</p>'
+  )
+})
 
-// My server is listening on port 3000
+// set the express server to listen on on port 3000 for GET request
 app.listen(3000, function() {
-  console.log('Server started on port 3000');
-});
+  console.log('Server started on port 3000')
+})
