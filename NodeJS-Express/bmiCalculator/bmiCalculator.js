8acca118470075df7484fea8aad1258816191922
pascaldevops <pascaldevops@gmail.com>
2019-03-07 13:13:18 -0500

    revised and comment the codes

--- NodeJS-Express/bmiCalculator/bmiCalculator.js
+++ NodeJS-Express/bmiCalculator/bmiCalculator.js
@@ -1,33 +1,36 @@
 //jshint esversion: 6
 
-const express = require('express');
-const bodyParser = require('body-parser');
+// Load the following node modules
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
+// Create an app
+const app = express()
 
-app.use(bodyParser.urlencoded({extended: true}));
+// parse the data
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// html page client wants
-app.get('/bmiCalculator', function(req, res) {
+// setup the home route
+app.get('/', function(req, res) {
   // send index.html to the server
-  res.sendFile(__dirname + '/bmiCalculator.html');
-});
+  res.sendFile(__dirname + '/bmiCalculator.html')
+})
 
 // Send the css file as well
 app.get('/style.css', function(req, res) {
-  res.sendFile(__dirname + '/style.css');
-});
+  res.sendFile(__dirname + '/style.css')
+})
 
 app.post('/bmiCalculator', function(req, res) {
-  const weight = parseFloat(req.body.weight);
-  const heightInInches1 = parseFloat(req.body['height-feet']) * 12;
-  const heightInInches2 = parseFloat(req.body['height-inch']);
-  const result = (weight*703) / Math.pow((heightInInches1 + heightInInches2), 2);
+  const weight = parseFloat(req.body.weight)
+  const heightInInches1 = parseFloat(req.body['height-feet']) * 12
+  const heightInInches2 = parseFloat(req.body['height-inch'])
+  const result = (weight * 703) / Math.pow(heightInInches1 + heightInInches2, 2)
 
-  res.send(`Your BMI is ${result.toFixed(2)}`);
-});
+  res.send(`Your BMI is ${result.toFixed(2)}`)
+})
 
 // The port the server is listening for requests
 app.listen(3000, function() {
-  console.log('Server listens on port 3000');
-});
+  console.log('Server listens on port 3000')
+})
